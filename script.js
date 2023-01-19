a51623cf7a163371b715cafc128db4c22ba032d0
Takhir28 <tahir28129185@gmail.com>
2023-01-19 19:47:26 +0600

    feat: home work

---
+++ script.js
@@ -0,0 +1,18 @@
+/*********** String **********/
+
+let name = "Привет, Тахир";
+alert(name);
+
+/*********** Number **********/
+
+let age = `30 лет!`;
+
+alert(age);
+
+/*********** String **********/
+
+let str1 = 'Салам,';
+let  str2 ="Бишкек!";
+let hello = `${str1} ${str2}`;
+
+alert(hello);
